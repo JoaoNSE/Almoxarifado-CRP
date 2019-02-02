ac11495fd047828b572879e19550650e6fd5d311
JoaoNSE <nobreenrique@hotmail.com>
2019-02-02 16:49:51 -0300

    Sistema de login migrado para spring security
    O login agora utiliza spring security para impedir realizar o login e usuários e impedir o acesso de quem não está logado.

---
+++ src/main/java/br/ce/qxd/crp/security/SecurityConfig.java
@@ -0,0 +1,41 @@
+package br.ce.qxd.crp.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception{
+		http.authorizeRequests()
+		.anyRequest().authenticated()
+		.and().formLogin()
+		.loginPage("/login")
+		.loginProcessingUrl("/signin")
+		.usernameParameter("nome")
+		.passwordParameter("senha")
+		.defaultSuccessUrl("/")
+		.permitAll()
+		.and().logout()
+		.permitAll()
+		.and().csrf().disable();
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+		auth.inMemoryAuthentication()
+		.withUser("admin").password("admin123").roles("ADMIN");
+	}
+
+	@Override
+	public void configure(WebSecurity web) throws Exception{
+		web.ignoring().antMatchers("/css/**", "/fonts/**", "/img/**", "/js/**");
+	}
+	
+}
--- src/main/webapp/WEB-INF/view/login/login.jsp
+++ src/main/webapp/WEB-INF/view/login/login.jsp
@@ -24,6 +24,14 @@
 						<p>${msg}</p>
 					</div>
 				</c:if>
+				<c:if test="${not empty logoutMsg}">
+					<div class="alert alert-success alert-dismissible" role="alert">
+						<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+							<span aria-hidden="true">&times;</span>
+						</button>
+						<p>${logoutMsg}</p>
+					</div>
+				</c:if>
 				
 				<jsp:include page="form_login.jsp" />
 			</div>
